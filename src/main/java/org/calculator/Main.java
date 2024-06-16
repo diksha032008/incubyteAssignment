bef82723e0751bd22ea914a10cb1d64e6749e646
Rohit <Rohit@adidas.com>
2024-06-16 13:24:14 +0530

    Handle an Empty String and addition of Single or Two Numbers

---
+++ src/main/java/org/calculator/Main.java
@@ -0,0 +1,11 @@
+package org.calculator;
+
+public class Main {
+
+  public static void main(String[] args) {
+    StringCalculator calculator = new StringCalculator();
+    System.out.println(calculator.add(""));
+    System.out.println(calculator.add("1"));
+    System.out.println(calculator.add("1,2"));
+  }
+}
---
+++ src/main/java/org/calculator/StringCalculator.java
@@ -0,0 +1,19 @@
+package org.calculator;
+
+public class StringCalculator {
+
+  public int add(String numbers) {
+    if (numbers.isEmpty()) {
+      return 0;
+    }
+
+    String[] tokens = numbers.split(",");
+    int sum = 0;
+    for (String token : tokens) {
+      sum += Integer.parseInt(token);
+    }
+
+    return sum;
+  }
+}
+
